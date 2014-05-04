b502023eb8365a560435b179e957ea6e5e172066
query-string <atimofeev@reactant.ru>
2014-05-04 21:11:35 +0700

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504092443) do
+ActiveRecord::Schema.define(version: 20140504141116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,5 +104,10 @@ ActiveRecord::Schema.define(version: 20140504092443) do
   end
 
   add_index "reviews", ["cafe_id"], name: "index_reviews_on_cafe_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
