313ed91c907cfc0e971f41b235f72c9be8365bce
query-string <atimofeev@reactant.ru>
2014-05-05 11:47:44 +0700

    Show actions links on movie page

--- app/controllers/my/movies_controller.rb
+++ app/controllers/my/movies_controller.rb
@@ -8,17 +8,17 @@ class My::MoviesController < ApplicationController
 
   def wait
     current_user.waits << Movie.find(params[:movie_id])
-    redirect_to :back
+    redirect_to root_path
   end
 
   def hate
     current_user.hates << Movie.find(params[:movie_id])
-    redirect_to :back
+    redirect_to root_path
   end
 
   def watch
     current_user.watches << Movie.find(params[:movie_id])
-    redirect_to :back
+    redirect_to root_path
   end
 
 end
