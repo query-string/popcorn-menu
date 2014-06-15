14a0fd7021190e2c8037949d46e303d9365d92d0
query-string <atimofeev@reactant.ru>
2014-06-15 14:01:58 +0400

    Generate devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :user_waits
   has_many :waits, through: :user_waits, source: :movie
