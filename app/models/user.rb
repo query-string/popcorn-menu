1b2b28baae5eaf37a9d15424ff81d0ac4b7610e6
query-string <atimofeev@reactant.ru>
2014-06-15 14:36:13 +0400

    Add User model Facebook auth implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,15 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.find_for_facebook_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      #user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
 end
