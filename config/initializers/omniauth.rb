fb23ec299a6f9a1afee3048dbc878f3fbadf889d
query-string <atimofeev@reactant.ru>
2014-06-15 14:27:53 +0400

    Add provider and uid fields to User

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
+  # @TODO: Remove from repo and history
   provider :facebook, 1421805441432437, 'e64b7d76542f1adcb693228cb9e8a9c1',  {:scope => 'email,read_friendlists,manage_friendlists,publish_stream,offline_access,read_mailbox', :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" }}}
 
 end
