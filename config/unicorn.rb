39440f1844cab9ca2e924cc84024444f0c5097d4
query-string <atimofeev@reactant.ru>
2014-08-31 20:20:56 +0700

    Update Unicorn for Heroku deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,40 +1,22 @@
-deploy_to  = "/home/popcornmenu"
-rails_root = "#{deploy_to}/current"
-pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file= "#{deploy_to}/shared/unicorn.sock"
-log_file   = "#{rails_root}/log/unicorn.log"
-err_log    = "#{rails_root}/log/unicorn_error.log"
-old_pid    = pid_file + '.oldbin'
-
-
-worker_processes 1
-timeout 30
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
-listen socket_file, :backlog => 1024
-pid pid_file
-stderr_path err_log
-stdout_path log_file
-
-
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
-
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
-end
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.connection.disconnect!
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
   defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.establish_connection
 end
