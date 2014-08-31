da604a16d77090efc28eb649db94be6093cbed93
query-string <atimofeev@reactant.ru>
2014-08-31 22:15:53 +0700

    Remove schedule

--- config/schedule.rb
+++
@@ -1,7 +0,0 @@
-job_type :rakeubuntu, 'cd :path && RAILS_ENV=production bundle exec rake :task :output'
-
-set :job_template, "/bin/bash -l -c ':job'"
-
-every 1.hour do
-  rakeubuntu 'movies:import'
-end
