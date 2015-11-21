fe681d75be72adf7e87ad0b87a2263cdccc8be67
Alex Timofeev <atimofeev@reactant.ru>
2015-11-21 18:54:13 +0700

    Extract sensitive keys to .env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   # @TODO: Remove from repo and history
-  provider :facebook, 1421805441432437, 'e64b7d76542f1adcb693228cb9e8a9c1',  {:scope => 'email,read_friendlists,read_mailbox', :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" }}}
+  provider :facebook, ENV["FB_ID"], ENV["FB_KEY"],  {:scope => 'email,read_friendlists,read_mailbox', :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" }}}
 
 end
